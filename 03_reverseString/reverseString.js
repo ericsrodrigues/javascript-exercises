5a91bb2382880d61b3085d3276f6674d0fc70369
ericsrodrigues <ericsrodrigues1@gmail.com>
2024-04-21 16:47:50 -0300

    Finish exercises 01 to 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  return string.split("").reverse().toString().replaceAll(",","");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...num) {
+  return array.filter((arrayElement) => {
+    for (let i = 0; i < num.length; i++) {
+      num[i] == arrayElement;
+    }
+  });
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
